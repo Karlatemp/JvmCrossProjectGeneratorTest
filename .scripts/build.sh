683d1807696258c37b9bc2ec35903e91ef977bd6
Karlatemp <karlatemp@vip.qq.com>
2021-04-30 22:39:18 +0800

    count 9

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,7 +3,7 @@ mkdir cmake-build-debug
 cd cmake-build-debug
 rm -rf CMakeCache.txt
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=$PWD -Dcross_triple=$CROSS ..
-cmake --build . --target native -- -j 3
+cmake --build . --target testing -- -j 3
 ecode=$?
 
 mkdir bin
--- native/src/lib.cpp
+++ native/src/lib.cpp
@@ -1,3 +1,1 @@
 #include <jni.h>
-
-JNICALL void a() {}
